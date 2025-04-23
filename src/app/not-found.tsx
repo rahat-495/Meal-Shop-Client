d888bb2f2c8fc253ae043627a7765d090e86b9e4
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-04-24 01:57:23 +0600

    code updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
-const NotFoundPage = () => {
+import LottieRender from "@/components/shared/lottierender";
+import notFound from "@/assets/animations/not-found-404.json";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const NotFound = () => {
   return (
-    <div>
-      <h1 className="flex justify-center items-center min-h-screen">
-        404 Not Found
-      </h1>
+    <div className="relative">
+      <LottieRender animationData={notFound} className="h-screen" />
+      <Link href="/" className="fixed bottom-16 left-1/2 -translate-x-1/2">
+        <Button className="cursor-pointer">Back to Homepage</Button>
+      </Link>
     </div>
   );
 };
 
-export default NotFoundPage;
+export default NotFound;
