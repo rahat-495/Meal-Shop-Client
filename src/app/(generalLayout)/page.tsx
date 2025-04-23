474def8eddce4001b7d2d57069a41e1d876dc12e
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-04-24 01:23:16 +0600

    folder structure changes

--- src/app/(generalLayout)/page.tsx
+++ src/app/(generalLayout)/page.tsx
@@ -1,16 +1,17 @@
 import MealBoxFaq from "@/components/modules/home/faq/Faq";
 import Banner from "@/components/modules/home/hero/Banner";
 import FooterSection from "@/components/shared/footersection";
+import Navbar from "@/components/shared/Navbar";
 
 const HomePage = () => {
-
-    return (
-        <div className=''>
-            <Banner/>
-            <MealBoxFaq/>
-            <FooterSection/>
-        </div>
-    );
+  return (
+    <div className="">
+      <Navbar></Navbar>
+      <Banner />
+      <MealBoxFaq />
+      <FooterSection />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center min-h-screen">
+        404 Not Found
+      </h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
