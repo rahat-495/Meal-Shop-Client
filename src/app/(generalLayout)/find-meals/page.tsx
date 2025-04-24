e27b4364c32d964fe6d7d868f32ef89f26edc8a5
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-04-24 14:20:32 +0600

    layout changes

---
+++ src/app/(generalLayout)/find-meals/page.tsx
@@ -0,0 +1,15 @@
+import SectionHeading from "@/components/shared/sectionheading";
+
+const FindMealsPage = () => {
+  return (
+    <div className="py-6 lg:py-8">
+      <SectionHeading
+        title="Find Your Flavor"
+        subtitle="Discover meals that match your cravings.
+Fresh picks, every time you search."
+      ></SectionHeading>
+    </div>
+  );
+};
+
+export default FindMealsPage;
