d7a5837a24bc90ca5f0a44c5c0e816fdf4efb590
Sohan Razzak <>
2025-04-25 08:15:58 +0600

    product details page added

--- src/services/Products/index.ts
+++ src/services/Products/index.ts
@@ -20,3 +20,17 @@ export const getAllProducts = async (page?: string, limit?: string) => {
         return Error(error.message);
     }
 };
+
+// get all products
+export const getProductById = async (id: string) => {
+
+    try {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_API}/meals/${id}`);
+
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        return Error(error.message);
+    }
+};
