1098683beda7e889b1d28084585ef1f299941528
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-04-25 16:00:18 +0600

    meals page completed

--- src/services/Products/index.ts
+++ src/services/Products/index.ts
@@ -1,36 +1,38 @@
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
-"use server"
+"use server";
 
 // get all products
-export const getAllProducts = async (page?: string, limit?: string) => {
-
-    try {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_BASE_API}/meals?limit=${limit}&page=${page}`,
-            {
-                next: {
-                    tags: ["PRODUCT"],
-                },
-            }
-        );
-        const data = await res.json();
-        return data;
-    } catch (error: any) {
-        return Error(error.message);
-    }
+export const getAllProducts = async (
+  page: string,
+  limit: string,
+  searchTerm: string
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/meals?limit=${limit || 5}&page=${
+        page || 1
+      }&searchTerm=${searchTerm || ""}`,
+      {
+        next: {
+          tags: ["PRODUCT"],
+        },
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (error: any) {
+    return Error(error.message);
+  }
 };
 
 // get all products
 export const getProductById = async (id: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/meals/${id}`);
 
-    try {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_BASE_API}/meals/${id}`);
-
-        const data = await res.json();
-        return data;
-    } catch (error: any) {
-        return Error(error.message);
-    }
+    const data = await res.json();
+    return data;
+  } catch (error: any) {
+    return Error(error.message);
+  }
 };
